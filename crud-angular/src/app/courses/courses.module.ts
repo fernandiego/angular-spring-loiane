caa7bd0fb52e0b252d53487f6cedb04f43724992
fernandiego <fernandodiego.sn@gmail.com>
2022-12-13 17:43:01 -0300

    Aula 07 Loiane - Angular plus spring boot

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
+import { AppMaterialModule } from '../shared/app-material/app-material.module'
 
 
 @NgModule({
@@ -14,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
---
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
+})
+export class AppMaterialModule { }
