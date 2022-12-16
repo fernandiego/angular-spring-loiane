b049f9274e570ad554fcda0de304de60b1e25896
fernandiego <fernandodiego.sn@gmail.com>
2022-12-16 18:37:10 -0300

    country roads, take me home, to the place i belong

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -16,7 +16,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(5000),
+      // delay(5000),
       tap(courses => console.log(courses)));
   }
 }
