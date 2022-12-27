916926f79a260dd262193493289fe7caa2f6dca8
fernandiego <fernandodiego.sn@gmail.com>
2022-12-27 18:31:18 -0300

    Aula 18 finalizada

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
@@ -23,7 +24,9 @@ export class CoursesComponent implements OnInit {
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     // this.coursesService = new CoursesService();
     this.courses$ = this.coursesService.list()
@@ -44,5 +47,9 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route})
+    console.log('onAdd');
   }
 }
