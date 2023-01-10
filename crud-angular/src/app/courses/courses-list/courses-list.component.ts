469f81cd8c4555f59dff9c37f778e93da7968070
Fernando Fox <fernandodiego.sn@gmail.com>
2023-01-10 13:03:15 -0300

    Aula 24, ainda na metade

---
+++ crud-angular/src/app/courses/courses-list/courses-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss']
+})
+export class CoursesListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  readonly displayedColumns = ['_id','name', 'category', 'actions'];
+
+  constructor(private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+
+  }
+  onAdd(): void {
+    this.router.navigate(['new'], {relativeTo: this.route})
+    console.log('onAdd');
+  }
+}
