ecd7e9675dffcfe054c8ff09ea86ba8d1a4c7f52
Fernando Fox <fernandodiego.sn@gmail.com>
2023-01-19 21:04:27 -0300

    Configurando o controller e get por registro individual

--- crud-spring/src/main/java/com/fox/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/fox/crudspring/controller/CourseController.java
@@ -1,8 +1,10 @@
 package com.fox.crudspring.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +29,13 @@ public class CourseController {
     // @RequestMapping(method = RequestMethod.GET) forma alternativa de getmapping
     public @ResponseBody Iterable<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
--- crud-spring/src/main/java/com/fox/crudspring/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/fox/crudspring/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.fox.crudspring.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.fox.crudspring.model.Course;
 
-@Repository
 public interface CourseRepository extends CrudRepository<Course, Long> {
 
 }
