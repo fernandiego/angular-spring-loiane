d28ee736ab4018755020dfae4ee7648e20dfafff
Fernando Fox <fernandodiego.sn@gmail.com>
2023-02-16 20:48:34 -0300

    aula 29 finalizada, implementando o update

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
+import { Course } from './../model/course';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs/operators';
+import { first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +25,17 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    console.log(record);
+    if (record._id) {
+      console.log('update');
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`{this.API}/${record._id}`, record).pipe(first());
   }
 }
