6f3dbca40c8eb0b3e97d8f657f62b4b2ba883f68
Fernando Fox <fernandodiego.sn@gmail.com>
2023-02-22 16:50:13 -0300

    baixar novamente o projeto, java usando versão errada

--- crud-spring/src/main/java/com/fox/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/fox/crudspring/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.fox.crudspring.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,15 @@ public class CourseController {
                 return ResponseEntity.ok().body(updated);
             })
             .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+        .map(recordFound -> {
+            courseRepository.deleteById(id);
+            return ResponseEntity.noContent().<Void>build();
+        })
+        .orElse(ResponseEntity.notFound().build());
     }
 }
