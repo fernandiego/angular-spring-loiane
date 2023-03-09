a4786c327e23a56ed4c450b9e009b9da0f6bf2f7
Fernando Fox <fernandodiego.sn@gmail.com>
2023-03-09 12:01:26 -0300

    aula 35 validação por meio do backend

--- crud-spring/src/main/java/com/fox/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/fox/crudspring/model/Course.java
@@ -1,5 +1,6 @@
 package com.fox.crudspring.model;
 
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -7,8 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
+import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Pattern;
 
 @Data
 @Entity
@@ -19,9 +23,16 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @NotBlank
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end | Front-end")
+    @NotNull
     @Column(length = 10, nullable = false)
     private String category;
 }
