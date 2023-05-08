8bc594bbaedeb2b888cadc8802b6521f367a888d
Fernando Fox <fernandodiego.sn@gmail.com>
2023-05-08 20:18:46 -0300

    Aula 45 oneToMany

---
+++ crud-spring/src/main/java/com/fox/crudspring/model/Lesson.java
@@ -0,0 +1,16 @@
+package com.fox.crudspring.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+public class Lesson{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(length = 11, nullable = false)
+    private String youtubeUrl;
+}
